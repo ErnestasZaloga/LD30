93dae712d43fb53834dc0178dd2987e4eace6fd9
Ernyz <ernestas.zaloga@gmail.com>
2014-08-23 10:57:32 +0300

    First commit.

--- core/src/com/ld30/game/LD30.java
+++ core/src/com/ld30/game/LD30.java
@@ -2,26 +2,26 @@ package com.ld30.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class LD30 extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	
+	private SpriteBatch batch;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		Gdx.graphics.setDisplayMode(Gdx.graphics.getDisplayModes()[0]);
+		batch = new SpriteBatch(1000);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+		if (Gdx.input.isKeyPressed(Keys.ESCAPE)) {
+			Gdx.app.exit();
+		}
+		
+		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClear(Gdx.gl.GL_COLOR_BUFFER_BIT);
+	} 
 }
