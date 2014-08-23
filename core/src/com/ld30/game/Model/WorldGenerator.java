abc51de4b14564aa3b0a07e147fc379267f71e1d
Ernyz <ernestas.zaloga@gmail.com>
2014-08-23 15:21:06 +0300

    River is generated in the map.

--- core/src/com/ld30/game/Model/WorldGenerator.java
+++ core/src/com/ld30/game/Model/WorldGenerator.java
@@ -11,25 +11,47 @@ public class WorldGenerator {
 	public static Tile[][] generateMap(Assets assets) {
 		Tile[][] tiles = new Tile[16][16];
 		
-		//Fill tile array with grass
+		/*
+		 * Fill tile array with grass
+		 */
 		for(int x = 0; x < tiles.length; x++) {
 			for(int y = 0; y < tiles[0].length; y++) {
 				Tile t = new Grass(assets.grass, x*32, y*32);
 				tiles[x][y] = t;
 			}
 		}
-		//Make a river
-<<<<<<< HEAD
-=======
-		//int riverStart = (int) Math.floor(Math.random()*tiles[0].length+1);
->>>>>>> b972348d875f078174bc80e46c877b8fbe4aa062
+		
+		/*
+		 * Make a river
+		 */
 		Vector2 riverStart = new Vector2(0f, (float) Math.floor(Math.random()*tiles[0].length));
 		Tile t = new Water(assets.water, 32*riverStart.x, 32*riverStart.y);
 		tiles[(int)riverStart.x][(int)riverStart.y] = t;
 		
-		int riverEnd = (int) Math.floor(Math.random()*tiles[0].length);
-		Tile t1 = new Water(assets.water, 15*32, 32*riverEnd);
-		tiles[15][riverEnd] = t1;
+		Vector2 riverEnd = new Vector2(tiles.length-1, (float) Math.floor(Math.random()*tiles[0].length));
+		Tile t1 = new Water(assets.water, 32*riverEnd.x, 32*riverEnd.y);
+		tiles[(int)riverEnd.x][(int)riverEnd.y] = t1;
+		
+		float currentX = riverStart.x;
+		float currentY = riverStart.y;
+		
+		while(currentX != riverEnd.x || currentY != riverEnd.y) {
+			if(currentX < riverEnd.x) {
+				currentX++;
+			} else if(currentX > riverEnd.x) {
+				currentX--;
+			}
+			
+			if(currentY < riverEnd.y) {
+				currentY++;
+			} else if(currentY > riverEnd.y) {
+				currentY--;
+			}
+			
+			Tile tile = new Water(assets.water, 32*currentX, 32*currentY);
+			tiles[(int)currentX][(int)currentY] = tile;
+		}
+		
 		
 		return tiles;
 	}
