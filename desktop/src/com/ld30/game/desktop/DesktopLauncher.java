e39a7d3a8d68fad56d7f6eddd7fb3ebfe34f8016
Ernyz <ernestas.zaloga@gmail.com>
2014-08-23 09:21:01 +0300

    First commit

---
+++ desktop/src/com/ld30/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ld30.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ld30.game.LD30;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LD30(), config);
+	}
+}
