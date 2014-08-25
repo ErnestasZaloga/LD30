2ec4f0c675be4487849b709e916b72ec7199e20a
Paulius Jurgelevicius <Paulius@Pauliuss-MacBook.local>
2014-08-25 23:28:57 +0300

    loading of sounds and menu music

--- core/src/com/ld30/game/Assets.java
+++ core/src/com/ld30/game/Assets.java
@@ -1,6 +1,8 @@
 package com.ld30.game;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -35,6 +37,18 @@ public class Assets {
 	public final TextureRegion soldierIcon;
 	public final TextureRegion workerIcon;
 	
+	public final Music music;
+	
+	public final Sound sirena;
+	public final Sound troopDead;
+	public final Sound resourcesLost;
+	public final Sound blipSelect;
+	public final Sound click;
+	public final Sound cityDestroyed;
+	public final Sound workerPathFinished;
+	public final Sound troopPathFinished;
+	public final Sound resourcePathFinished;
+	
 	private final int tileWH = 8;
 	
 	public Assets () {
@@ -68,6 +82,19 @@ public class Assets {
 		soldierIcon = new TextureRegion(atlas.findRegion("KarysIkona"));
 		workerIcon = new TextureRegion(atlas.findRegion("DarbininkasIkona"));
 		
+		music = Gdx.audio.newMusic(Gdx.files.internal("music/Music.mp3"));
+		music.play();
+		music.setLooping(true);
+		
+		sirena = Gdx.audio.newSound(Gdx.files.internal("sounds/8bit sirenos.wav"));
+		troopDead = Gdx.audio.newSound(Gdx.files.internal("sounds/atimimo1.wav"));
+		resourcesLost = Gdx.audio.newSound(Gdx.files.internal("sounds/atimimo3.wav"));
+		blipSelect = Gdx.audio.newSound(Gdx.files.internal("sounds/Blip_Select10.wav"));
+		click = Gdx.audio.newSound(Gdx.files.internal("sounds/click5.wav"));
+		cityDestroyed = Gdx.audio.newSound(Gdx.files.internal("sounds/sugriuvimas2.wav"));
+		workerPathFinished = Gdx.audio.newSound(Gdx.files.internal("sounds/unituIejimas1.wav"));
+		troopPathFinished = Gdx.audio.newSound(Gdx.files.internal("sounds/unituIejimas2.wav"));
+		resourcePathFinished = Gdx.audio.newSound(Gdx.files.internal("sounds/unituIejimas3.wav"));
 	}
 	
 	public void dispose () {
