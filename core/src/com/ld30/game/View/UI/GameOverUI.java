ce61cc0197f92b1a6cfd874c52669d14f4fdb2a0
Vytautas <vytautas.sedelskis@gmail.com>
2014-08-25 21:44:30 +0300

    ui

--- core/src/com/ld30/game/View/UI/GameOverUI.java
+++ core/src/com/ld30/game/View/UI/GameOverUI.java
@@ -1,18 +1,38 @@
 package com.ld30.game.View.UI;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.utils.Array;
+import com.ld30.game.Assets;
+import com.ld30.game.Model.City;
 
 public class GameOverUI extends Group{
-	private final Image gameOver;
 	private final Image background;
+	private final Label gameOver;
+	
+	private final Skin skin;
 	
 	private int score;
 	
-	public GameOverUI(){
-		background = new Image();
+	public GameOverUI(Assets assets, Array<City> cities){
+		skin = assets.UISkin;
 		
-		gameOver = new Image();
+		background = new Image(assets.black);
+		addActor(background);
+		
+		
+		gameOver = new Label("GAME OVER", skin);
+		gameOver.setFontScale(5);
+		gameOver.getColor().r = 0.0f;
+		gameOver.getColor().g = 0.0f;
+		gameOver.getColor().b = 0.0f;
+		gameOver.pack();
+		addActor(gameOver);
+	}
+	
+	public void update() {
 		
 	}
 	
@@ -21,6 +41,7 @@ public class GameOverUI extends Group{
 		super.setSize(width, height);
 		
 		background.setSize(width, height);
+		gameOver.setPosition((width - gameOver.getWidth()) / 2, (height - gameOver.getHeight()) / 2 + height / 4);
 		
 	}
 
